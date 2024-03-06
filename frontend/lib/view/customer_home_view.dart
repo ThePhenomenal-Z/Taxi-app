2919e6728677a90eb5a38c97dc0ff516e3fcc631
mohamed majzoub <mohamed13majzoub@gmail.com>
2024-03-06 11:13:26 +0300

    front 1

---
+++ frontend/lib/view/customer_home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomerHomeView extends StatefulWidget {
+  const CustomerHomeView({super.key});
+
+  @override
+  State<CustomerHomeView> createState() => _CustomerHomeViewState();
+}
+
+class _CustomerHomeViewState extends State<CustomerHomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
